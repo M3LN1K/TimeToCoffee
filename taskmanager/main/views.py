5e8cd069ea18b0de54502f423f324aab3a1e1dc4
M3LN1K <whynot-2@yandex.ru>
2022-05-20 21:55:43 +0300

    change_one_and_lust

---
+++ taskmanager/main/views.py
@@ -0,0 +1,136 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+
+def anime(request):
+    return render(request, "main/anime.html")
+
+
+def china_coffee(request):
+    return render(request, "main/china_coffee.html")
+
+
+def coffee(request):
+    return render(request, "main/coffee.html")
+
+
+def cosplay(request):
+    return render(request, "main/cosplay.html")
+
+
+def loging(request):
+    return render(request, "main/loging.html")
+
+
+def japanese_tea(request):
+    return render(request, "main/japanese_tea.html")
+
+
+def restaurant(request):
+    return render(request, "main/restaurant.html")
+
+
+def tea(request):
+    return render(request, "main/tea.html")
+
+
+def americano(request):
+    return render(request, "main/Americano.html")
+
+
+def bicherin(request):
+    return render(request, "main/Bicherin.html")
+
+
+def breve(request):
+    return render(request, "main/Breve.html")
+
+
+def cappuccino(request):
+   return render(request, "main/Cappuccino.html")
+
+
+def con_panna(request):
+    return render(request, "main/Con_panna.html")
+
+
+def corretto(request):
+    return render(request, "main/Corretto.html")
+
+
+def doppio(request):
+    return render(request, "main/Doppio.html")
+
+
+def espresso(request):
+    return render(request, "main/Espresso.html")
+
+
+def flat_white(request):
+    return render(request, "main/Flat_White.html")
+
+
+def fredo(request):
+    return render(request, "main/Fredo.html")
+
+
+def glasse(request):
+    return render(request, "main/Glasse.html")
+
+
+def honey_raf(request):
+    return render(request, "main/Honey_Raf.html")
+
+
+def irish(request):
+    return render(request, "main/Irish.html")
+
+
+def latte(request):
+    return render(request, "main/Latte.html")
+
+
+def latte_macchiato(request):
+    return render(request, "main/Latte_Macchiato.html")
+
+
+def lungo(request):
+    return render(request, "main/Lungo.html")
+
+
+def macchiato(request):
+    return render(request, "main/Macchiato.html")
+
+
+def marochino(request):
+    return render(request, "main/Marochino.html")
+
+
+def mocha(request):
+    return render(request, "main/Mocha.html")
+
+
+def raf_coffee(request):
+    return render(request, "main/Raf_coffee.html")
+
+
+def ristretto(request):
+    return render(request, "main/Ristretto.html")
+
+
+def romano(request):
+    return render(request, "main/Romano.html")
+
+
+def triplo(request):
+    return render(request, "main/Triplo.html")
+
+
+def viennese_coffee(request):
+    return render(request, "main/Viennese_coffee.html")
+
+
+
